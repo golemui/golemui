4b36e83dc0ec2f69296570d51142436b0cf16424
Mud Scientist <mudscientist@golemui.com>
2025-09-19 22:02:07 +0200

    Create angular projects

---
+++ libs/angular-vanilla/src/lib/angular-vanilla/angular-vanilla.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-vanilla',
+  imports: [],
+  templateUrl: './angular-vanilla.html',
+  styleUrl: './angular-vanilla.scss',
+})
+export class AngularVanilla {}
