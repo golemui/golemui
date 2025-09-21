1a783cce80981b655daf2ee4f5adea64a6a128d1
Mud Scientist <mudscientist@golemui.com>
2025-09-21 21:57:31 +0200

    CI fixes

--- apps/angular-playground/src/app/app.spec.ts
+++ apps/angular-playground/src/app/app.spec.ts
@@ -1,20 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
+      imports: [App],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should render', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-playground'
-    );
+    expect(compiled).toBeTruthy();
   });
 });
