9ab10372ecbf7a985033169ca8f79c1cfb5b249d
Mud Scientist <mudscientist@golemui.com>
2025-09-28 20:43:19 +0200

    Integrate custom heading with adapters

--- apps/react-playground/src/app/custom-fields/heading/heading.component.tsx
+++ apps/react-playground/src/app/custom-fields/heading/heading.component.tsx
@@ -1,11 +1,31 @@
+import * as Core from '@formforge/core';
+import { useField } from '@formforge/react';
+import { createElement, PropsWithChildren } from 'react';
 import styles from './heading.component.module.scss';
 
-export function HeadingComponent() {
+type OwnWidgetProps = {
+  text: string;
+  level?: number;
+};
+
+export function HeadingComponent(fieldInstance: Core.WithField) {
+  const field = fieldInstance.field as Core.Field;
+  const { uid, props } = useField<OwnWidgetProps>(field);
+
   return (
-    <div className="field">
-      <h1 className={styles.heading}>Heading</h1>
+    <div className="field" id={uid}>
+      <DynamicHeading level={props.level || 1}>{props.text}</DynamicHeading>
     </div>
   );
+}
+
+function DynamicHeading({
+  level,
+  children,
+}: PropsWithChildren<{ level: number }>) {
+  const HeadingTag = `h${level}`;
+
+  return createElement(HeadingTag, { className: styles.heading }, children);
 }
 
 export default HeadingComponent;
