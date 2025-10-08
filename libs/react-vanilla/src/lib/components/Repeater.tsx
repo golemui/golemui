fb0488a758d2abb46eda4777c28be3b8977e0c81
Raul Jimenez <elecash@gmail.com>
2025-10-08 18:12:42 +0200

    feat(repeater): Add buttons to react repeater component

--- libs/react-vanilla/src/lib/components/Repeater.tsx
+++ libs/react-vanilla/src/lib/components/Repeater.tsx
@@ -11,7 +11,21 @@ export function Repeater(fieldInstance: Core.WithField) {
   const field = fieldInstance.field as Core.ControlField<
     Record<string, unknown>[]
   >;
-  const { uid, value, label, formContext } = useControl(field);
+  const { uid, value, label, formContext, onValueChanged } = useControl(field);
+
+  const addItem = useCallback((value: Record<string, unknown>[]) => {
+    const newValue = [...value ?? [], {}];
+    onValueChanged(newValue);
+  }, [onValueChanged]);
+
+  const removeItem = useCallback(
+    (value: Record<string, unknown>[], index: number) => {
+      const arr = [...value ?? []]
+      arr.splice(index, 1);
+      onValueChanged(arr);
+    },
+    [onValueChanged]
+  );
 
   const renderFields = useCallback(() => {
     return value?.map((_, index) => {
@@ -19,10 +33,11 @@ export function Repeater(fieldInstance: Core.WithField) {
         <RepeaterIndexContext.Provider value={index}>
           <FieldRenderer
             key={`${uid}-${index}`}
-            field={field.props?.['template']}
+            field={field.props?.template}
             repeaterIndex={index}
             formContext={formContext}
           />
+          <button type="button" onClick={() => removeItem(value, index)}>{field.props?.removeLabel ?? 'Remove'}</button>
         </RepeaterIndexContext.Provider>
       );
     });
@@ -32,6 +47,7 @@ export function Repeater(fieldInstance: Core.WithField) {
     <div className="field" id={uid}>
       {label && <h2 key={`${uid}-title`}>{label}</h2>}
       {renderFields()}
+      <button type="button" onClick={() => addItem(value)}>{field.props?.addLabel ?? 'Add'}</button>
     </div>
   );
 }
