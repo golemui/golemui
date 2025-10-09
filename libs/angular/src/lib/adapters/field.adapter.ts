a545d64e1f9a88c3de554674f8854a7d597cfe2d
Mud Scientist <mudscientist@golemui.com>
2025-10-09 12:21:35 +0200

    fix(angular): Move common adapter functionality to a BaseAdapter class

--- libs/angular/src/lib/adapters/field.adapter.ts
+++ libs/angular/src/lib/adapters/field.adapter.ts
@@ -1,64 +1,23 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import * as Core from '@formforge/core';
-import { Subject, takeUntil } from 'rxjs';
-import { AngularFormContext } from '../context/form.context';
+import { BaseAdapter } from './base.adapter';
 
 @Injectable()
-export class FieldAdapter<ExtraProps extends Record<string, any>> {
-  private context = inject(AngularFormContext);
-  private destroy$ = new Subject<void>();
-  private field!: Core.Field;
-
+export class FieldAdapter<
+  ExtraProps extends Record<string, any>,
+> extends BaseAdapter<Core.Field> {
   templateData = signal<ExtraProps>({} as ExtraProps);
 
   init(field: Core.Field) {
     this.field = field;
 
-    this.templateData.update((value) => ({
-      ...value,
+    // Set initial templateData
+    this.templateData.update((current) => ({
+      ...current,
       ...this.field.props,
     }));
 
-    this.context.store.dispatch({
-      type: 'ADD_FIELD',
-      payload: { field },
-    });
-
-    this.context.store.state$
-      .pipe(takeUntil(this.destroy$), Core.currentStates)
-      .subscribe(() => {
-        const props = this.field.props;
-        if (props !== undefined) {
-          // we dont want 'label.register', we only want the base keys 'label' (even if they are not set)
-          const uniquePropsWithoutState = Array.from(
-            new Set(
-              Object.keys(props).map((prop) => prop.split('.')[0]),
-            ).keys(),
-          );
-          const updatedProps = uniquePropsWithoutState.reduce(
-            (templateData, key: keyof ExtraProps) => {
-              templateData[key] = this.context.getPropertyValueByCurrentState(
-                key as string,
-                props,
-              ) as any;
-              return templateData;
-            },
-            {} as ExtraProps,
-          );
-
-          this.templateData.update((value) => ({
-            ...value,
-            ...updatedProps,
-          }));
-        }
-      });
-  }
-
-  destroy() {
-    this.context.store.dispatch({
-      type: 'REMOVE_FIELD',
-      payload: { uid: this.field.uid },
-    });
-    this.destroy$.next();
+    this.addFieldToTheStore(field);
+    this.propsUpdaterByCurrentState(this.templateData);
   }
 }
--- libs/angular/src/lib/adapters/layout.adapter.ts
+++ libs/angular/src/lib/adapters/layout.adapter.ts
@@ -1,23 +1,24 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import * as Core from '@formforge/core';
-import { combineLatest, map, Observable, of, Subject, takeUntil } from 'rxjs';
-import { AngularFormContext } from '../context/form.context';
+import { combineLatest, map, of, takeUntil } from 'rxjs';
+import { BaseAdapter } from './base.adapter';
 
 @Injectable()
-export class LayoutAdapter {
-  private context = inject(AngularFormContext);
-  private destroy$ = new Subject<void>();
-  private field!: Core.LayoutField;
+export class LayoutAdapter extends BaseAdapter<Core.LayoutField> {
   children$ = of<Core.FormField<string>[]>([]);
 
   init(field: Core.LayoutField) {
     this.field = field;
-    const children: Observable<Core.FormField<string>[]> = of(field.children);
-    const selectFieldFlags = this.context.store.state$.pipe(
+
+    const fieldFlagsSelector = this.context.store.state$.pipe(
       Core.selectFieldFlags,
     );
 
-    this.children$ = combineLatest([children, selectFieldFlags]).pipe(
+    // Listen to the fieldFlags stream and filter the layout's `children` based on their `hidden` flag
+    this.children$ = combineLatest([
+      of(field.children),
+      fieldFlagsSelector,
+    ]).pipe(
       takeUntil(this.destroy$),
       map(([children]) => {
         const fieldFlags = this.context.store.getState().fieldFlags;
@@ -29,17 +30,6 @@ export class LayoutAdapter {
       }),
     );
 
-    this.context.store.dispatch({
-      type: 'ADD_FIELD',
-      payload: { field },
-    });
-  }
-
-  destroy() {
-    this.context.store.dispatch({
-      type: 'REMOVE_FIELD',
-      payload: { uid: this.field.uid },
-    });
-    this.destroy$.next();
+    this.addFieldToTheStore(field);
   }
 }
