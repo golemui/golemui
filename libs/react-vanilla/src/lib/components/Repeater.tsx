29ae8328bc0dcf20b7ad3e2b204428fa6ec31133
Raul Jimenez <elecash@gmail.com>
2025-10-12 12:17:16 +0200

    Fix repeater styles

--- libs/react-vanilla/src/lib/components/Repeater.tsx
+++ libs/react-vanilla/src/lib/components/Repeater.tsx
@@ -62,7 +62,7 @@ export function Repeater(fieldInstance: Core.WithField) {
 
   return (
     <div className="ff-repeater">
-      <div className="card" id={uid}>
+      <div id={uid}>
         {label && <h2 key={`${uid}-title`}>{label}</h2>}
         {renderFields()}
         <button type="button" onClick={() => addItem(value || [])} disabled={props.limit ? props.limit === (value?.length ?? 0) : false}
