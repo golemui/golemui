e287842fcaab75d85010eca4529d21acf9d8aeb7
Raul Jimenez <elecash@gmail.com>
2025-10-17 11:14:38 +0200

    Add textinput to LitElement implementation

--- libs/lit-vanilla/src/lib/components/textinput.element.ts
+++ libs/lit-vanilla/src/lib/components/textinput.element.ts
@@ -1,17 +1,52 @@
-import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { LitElement, html, nothing } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import * as Core from '@formforge/core';
+import { consume, provide } from '@lit/context';
+import * as Lit from '@formforge/lit';
 
 @customElement('ff-textinput')
 export class TextinputElement extends LitElement implements Core.WithField {
-  field!: Core.Field;
+  field!: Core.ControlField<string>;
+
+  @consume({ context: Lit.formContext })
+  @property({ attribute: false })
+  formContext!: Lit.LitFormContext<any>;
+
+  @provide({ context: Lit.controlContext })
+  adapter = new Lit.ControlAdapter();
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.adapter.context = this.formContext;
+    this.adapter.init(this.field);
+  }
 
   override render() {
     super.render();
     return html`
       <div class="field">
-        <input type="text" />
+        <label [for]="field.uid"
+          >${this.adapter.templateData['label'] +
+          (this.adapter.templateData['required'] ? ' *' : '')}</label
+        >
+        <input
+          type="text"
+          id=${this.field.uid}
+          value=${this.adapter.templateData['value'] ?? ''}
+          disabled=${this.adapter.templateData['disabled'] || nothing}
+          @input="${this.valueChanged}"
+        />
       </div>
     `;
+  }
+
+  valueChanged(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.adapter.valueChanged(target.value);
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.adapter.destroy();
   }
 }
