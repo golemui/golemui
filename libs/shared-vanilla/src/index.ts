7ff41daeecaf59087abc05e32542056b20a84468
Mud Scientist <mudscientist@golemui.com>
2025-11-05 16:25:57 +0100

    Move Control and Layout adapters templateData interface

--- libs/shared-vanilla/src/index.ts
+++ libs/shared-vanilla/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/props';
-export * from './lib/layout';
-export * from './lib/control';
 export * from './mocks/kitchen-sink';
 export * from './mocks/selects';
 export * from './mocks/signin';
