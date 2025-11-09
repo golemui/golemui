7c9934816b595d8ad5f24c2aee3fcbc5e9521a6b
Mud Scientist <mudscientist@golemui.com>
2025-11-09 20:00:48 +0100

    Add json schema mock

--- libs/shared-vanilla/src/index.ts
+++ libs/shared-vanilla/src/index.ts
@@ -1,4 +1,6 @@
-export * from './lib/props';
+export * from './lib/field.factory';
+export * from './lib/field.props';
+export * from './mocks/json-schema-test';
 export * from './mocks/kitchen-sink';
 export * from './mocks/selects';
 export * from './mocks/signin';
