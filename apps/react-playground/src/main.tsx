20769bed7b5c4208898905e2837267725c550581
Raul Jimenez <elecash@gmail.com>
2025-11-13 17:14:23 +0100

    styles: Use sass instead of plain css

--- apps/react-playground/src/main.tsx
+++ apps/react-playground/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-import '@libs/core/src/styles/index.css';
+import '@libs/core/src/styles/index.scss';
 
 import { BrowserRouter } from 'react-router';
 
