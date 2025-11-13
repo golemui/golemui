2e773105ca21d7a36257000114e981b445b3ebf1
Raul Jimenez <elecash@gmail.com>
2025-11-13 17:26:25 +0100

    styles: Move styles from core to shared-vanilla

--- apps/react-playground/src/main.tsx
+++ apps/react-playground/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
-import '@libs/core/src/styles/index.scss';
 
 import { BrowserRouter } from 'react-router';
 
