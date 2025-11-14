65ffa47ff3c6f726f538395819c4310294b14db0
Mud Scientist <mudscientist@golemui.com>
2025-11-14 16:11:04 +0100

    triple eq

--- apps/apps-shared/src/lib/custom-validators/allowed-names.ts
+++ apps/apps-shared/src/lib/custom-validators/allowed-names.ts
@@ -4,7 +4,7 @@ import * as z from 'zod/mini';
 export const allowedNames: CustomValidatorSchemaFn = (names: string[]) =>
   z.string().check(
     z.superRefine((val, ctx) => {
-      if (names.includes(val) == false) {
+      if (names.includes(val) === false) {
         ctx.addIssue({
           code: 'custom',
           message: `Name "${val}" not in ${names.map((name) => `"${name}"`).join(', ')}`,
