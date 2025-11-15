dcf9849b504e74ca9afa197d600bdfee7811a15a
Raul Jimenez <elecash@gmail.com>
2025-11-15 12:48:46 +0100

    fix(react): add css classes for validation errors

--- libs/react-vanilla/src/lib/components/shared/Errors.tsx
+++ libs/react-vanilla/src/lib/components/shared/Errors.tsx
@@ -3,7 +3,7 @@ export function Errors({ errors }: { errors: string[] }) {
     return (
       <ul>
         {errors.map((error) => (
-          <li>{error}</li>
+          <li className="gui-error">{error}</li>
         ))}
       </ul>
     );
