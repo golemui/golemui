895faf61ff403f66126445369938288f2cbe69a7
Raul Jimenez <elecash@gmail.com>
2025-11-16 11:09:23 +0100

    fix: remove access public on release publish

--- tools/release.ts
+++ tools/release.ts
@@ -61,7 +61,6 @@ async function copyChangelogFiles() {
 
   const publishResult = await releasePublish({
     registry: 'https://registry.npmjs.org/',
-    access: 'public',
   });
   process.exit(Object.values(publishResult).every((result) => result.code === 0) ? 0 : 1);
 })();
