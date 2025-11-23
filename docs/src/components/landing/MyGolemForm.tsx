134d6916aa87f6bee295feed4410264b8530b06e
Raul Jimenez <elecash@gmail.com>
2025-11-23 20:33:24 +0100

    Update website

--- docs/src/components/landing/MyGolemForm.tsx
+++ docs/src/components/landing/MyGolemForm.tsx
@@ -1,5 +1,5 @@
-import { FormComponent } from '@golemui/react';
 import { vanillaFieldLoaders } from '@golemui/react-vanilla';
+import { FormComponent } from '@golemui/react';
 
 export default function MyGolemForm(formDef: any, data: any) {
   return <FormComponent formDef={formDef} data={data} fieldLoader={{ ...vanillaFieldLoaders }} />;
