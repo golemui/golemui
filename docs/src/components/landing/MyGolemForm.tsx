b46609f2bc426a4938e43dd00561bee2734fffb3
Raul Jimenez <elecash@gmail.com>
2025-11-26 16:25:32 +0100

    Update packages

--- docs/src/components/landing/MyGolemForm.tsx
+++
@@ -1,6 +0,0 @@
-import { vanillaFieldLoaders } from '@golemui/react-vanilla';
-import { FormComponent } from '@golemui/react';
-
-export default function MyGolemForm(formDef: any, data: any) {
-  return <FormComponent formDef={formDef} data={data} fieldLoader={{ ...vanillaFieldLoaders }} />;
-}
