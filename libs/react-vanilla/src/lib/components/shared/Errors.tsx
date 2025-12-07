2fc9305f06544d22f70fe535fc11d744ec49b746
Mud Scientist <mudscientist@golemui.com>
2025-12-07 18:21:53 +0100

    fix(react): fix errors key

--- libs/react-vanilla/src/lib/components/shared/Errors.tsx
+++ libs/react-vanilla/src/lib/components/shared/Errors.tsx
@@ -1,8 +1,8 @@
 export function Errors({ errors, uid }: { errors: string[]; uid: string }) {
   return (
     <ul className="gui-validator" id={`${uid}_errors`}>
-      {errors.map((error) => (
-        <li className="gui-validator__error" role="status">
+      {errors.map((error, index) => (
+        <li className="gui-validator__error" role="status" key={index}>
           {error}
         </li>
       ))}
