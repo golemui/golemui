49b8f636f30cb1c78c00974c48b5e7512542ad55
Raul Jimenez <elecash@gmail.com>
2025-12-14 12:21:20 +0100

    test(angular): add angular mount test

--- libs/react-vanilla/src/mount.cy.tsx
+++ libs/react-vanilla/src/mount.cy.tsx
@@ -1,6 +1,6 @@
 import { mount } from 'cypress/react';
 import { runAlertComponentTests } from '@golemui/ui-testing';
-import { FormComponent } from '../src/lib/components/Form';
+import { FormComponent } from './lib/components/Form';
 
 const mountReact = (formDef: Record<string, any>) => {
   mount(<FormComponent formDef={{ formDef }} />);
