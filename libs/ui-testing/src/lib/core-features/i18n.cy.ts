599b55c63e248b84f540d79492f654c5b98ed7bd
Mud Scientist <mudscientist@golemui.com>
2026-01-20 12:52:49 +0100

    tests: fix i18n test

--- libs/ui-testing/src/lib/core-features/i18n.cy.ts
+++ libs/ui-testing/src/lib/core-features/i18n.cy.ts
@@ -14,9 +14,18 @@ i18next.init({
 });
 
 export const i18nTranslator: Core.I18nTranslator = {
+  get lang() {
+    return i18next.language;
+  },
   translate(key: Core.TranslationKey, params?: Core.I18nParams, defaultValue?: string): string {
-    console.log('aaaa', { key, params });
     return i18next.t(key, { ...params, defaultValue });
+  },
+  subscribe(listener: (lang: string) => void) {
+    const onLanguageChanged = (lng: string) => listener(lng);
+    i18next.on('languageChanged', onLanguageChanged);
+    return () => {
+      i18next.off('languageChanged', onLanguageChanged);
+    };
   },
 };
 
