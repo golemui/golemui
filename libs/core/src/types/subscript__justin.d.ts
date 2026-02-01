5ca72f21b74ce7b238e6bed6390d328ca81e4f2d
Mud Scientist <mudscientist@golemui.com>
2026-02-01 15:51:48 +0100

    build(subscript): Upgrade to v10

--- libs/core/src/types/subscript__justin.d.ts
+++ libs/core/src/types/subscript__justin.d.ts
@@ -1,3 +1,4 @@
+// TODO: This shouldn't be required. There's something in this project's config that breaks subscript's typings
 declare module 'subscript/justin' {
   type OperatorFunction = (...args: any[]) => any;
 
