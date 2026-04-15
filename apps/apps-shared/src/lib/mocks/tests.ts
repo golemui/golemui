bd30829c378f2832654ced11a87ca4244ca049db
Mud Scientist <mudscientist@golemui.com>
2026-04-15 23:44:25 +0200

    example

--- apps/apps-shared/src/lib/mocks/tests.ts
+++ apps/apps-shared/src/lib/mocks/tests.ts
@@ -14,10 +14,16 @@ const getFormDefinition = () =>
         kind: 'input',
         type: 'textinput',
         path: 'userName',
+        validator: {
+          type: 'string',
+          required: true,
+          minLength: 3,
+        },
       },
       {
         uid: 'is-vip',
         kind: 'input',
+        label: 'Is VIP',
         type: 'checkbox',
         path: 'isVip',
       },
@@ -29,6 +35,34 @@ const getFormDefinition = () =>
           text: 'Welcome VIP: {{$form.userName}}',
         },
         include: { in: ['vip'] },
+      },
+      {
+        uid: 'alert1',
+        kind: 'display',
+        type: 'alert',
+        props: {
+          text: 'One Error: {{$errors.userName}}',
+        },
+        include: { when: '$errors.userName?.length === 1' },
+      },
+      {
+        uid: 'alert2',
+        kind: 'display',
+        type: 'alert',
+        props: {
+          text: 'Two Errors: {{$errors.userName}} becasue form is invalid: {{$formIsInvalid}}',
+        },
+        include: { when: '$errors.userName?.length === 2' },
+      },
+      {
+        uid: 'button',
+        kind: 'action',
+        type: 'button',
+        label: 'Send',
+        disabled: { when: '$formIsInvalid' },
+        on: {
+          click: 'send',
+        },
       },
     ],
   });
