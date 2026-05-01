ad1045cd4bf46bc9a6ef7cd9769aca8d481ad1ee
Mud Scientist <mudscientist@golemui.com>
2026-05-01 17:27:38 +0200

    add devMode activation logic to our playtgrounds

--- apps/angular-playground/src/main.ts
+++ apps/angular-playground/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { enableDevMode } from '@golemui/core';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+
+if (environment.env !== 'prod') {
+  console.log('[GolemUI] DEV mode is enabled');
+  enableDevMode();
+}
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
--- apps/lit-playground/src/main.ts
+++ apps/lit-playground/src/main.ts
@@ -1,1 +1,7 @@
+import { enableDevMode } from '@golemui/core';
 import './app/app.element';
+
+if (import.meta.env.DEV) {
+  console.log('[GolemUI] DEV mode is enabled');
+  enableDevMode();
+}
