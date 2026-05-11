840c144e77160883466db4e71a78621106cc163c
Raúl Jiménez <elecash@gmail.com>
2026-05-11 16:08:31 +0200

    docs: remove form studio (#402)
    * docs: remove form studio
    
    * ci: remove monaco dependency

--- apps/form-studio/src/worker.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'monaco-editor/esm/vs/editor/editor.worker?worker' {
-  const WorkerConstructor: new () => Worker;
-  export default WorkerConstructor;
-}
-
-declare module 'monaco-editor/esm/vs/language/json/json.worker?worker' {
-  const WorkerConstructor: new () => Worker;
-  export default WorkerConstructor;
-}
