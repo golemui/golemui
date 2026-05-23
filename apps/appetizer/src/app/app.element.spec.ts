aa4ae18b8b326b4ce668bd0caff087c05def212c
Mud Scientist <mudscientist@golemui.com>
2026-05-23 19:46:13 +0200

    fix: lint error

--- apps/appetizer/src/app/app.element.spec.ts
+++ apps/appetizer/src/app/app.element.spec.ts
@@ -10,9 +10,13 @@ describe('AppElement', () => {
         matches: false,
         media: query,
         onchange: null,
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         addListener: () => {},
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         removeListener: () => {},
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         addEventListener: () => {},
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
         removeEventListener: () => {},
         dispatchEvent: () => false,
       }),
