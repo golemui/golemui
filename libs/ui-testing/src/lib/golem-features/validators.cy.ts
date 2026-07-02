143c08d0144ed39368b847d6122ad653c03e76df
Raúl Jiménez <elecash@gmail.com>
2026-07-02 04:27:53 -0400

    test: fix racing condition at startup in the validator tests

--- libs/ui-testing/src/lib/golem-features/validators.cy.ts
+++ libs/ui-testing/src/lib/golem-features/validators.cy.ts
@@ -41,6 +41,7 @@ export const runValidatorsComponentTests = (mountFn: MountComponentFn) => {
             ],
           }),
         });
+        cy.get('[data-cy="requiredString_textinput"]').should('exist');
         cy.get('[data-cy="testButton_button"]').click();
         cy.get('[data-cy="requiredString_validator-errors"]').should('exist');
         cy.get('[data-cy="requiredString_validator-error"]').contains(
