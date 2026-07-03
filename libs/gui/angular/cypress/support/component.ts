51092151c174fb768e1b22232341f64df0b30844
Raúl Jiménez <elecash@gmail.com>
2026-07-03 15:55:26 +0200

    test: disable enforce module boundaries errors for css files

--- libs/gui/angular/cypress/support/component.ts
+++ libs/gui/angular/cypress/support/component.ts
@@ -1,20 +1,4 @@
 import { mount } from 'cypress/angular';
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.ts using ES2015 syntax:
 import { memoryCleaner } from '@golemui/ui-testing';
 import './commands';
 
--- libs/gui/react/cypress/support/component.ts
+++ libs/gui/react/cypress/support/component.ts
@@ -1,20 +1,4 @@
 import { mount } from 'cypress/react';
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.ts using ES2015 syntax:
 import '../../../components/src/styles/index.scss';
 import { memoryCleaner } from '@golemui/ui-testing';
 import './commands';
